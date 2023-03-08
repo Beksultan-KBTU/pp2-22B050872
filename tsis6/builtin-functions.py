1e09d0fb0f1b36014c81f8ee59fcaff2da17489b
Beksultan <bekamega2004@gmail.com>
2023-03-08 23:12:01 +0600

    Sixth commit

---
+++ tsis6/builtin-functions.py
@@ -0,0 +1,31 @@
+# problem 1
+# import math
+# a = list(map(int, input().split()))
+# b = math.prod(a)
+# print(b)
+
+# problem 2
+# s = input()
+# lower = sum(map(str.islower, s))
+# upper = sum(map(str.isupper, s))
+# print(upper)
+# print(lower)
+
+# problem 3
+# s = input()
+# if s[::-1] == s:
+# 	print('Palindrom')
+# else:
+# 	print('No')
+
+# problem 4
+# import time
+# print('Sample Input')
+# number = int(input())
+# milis = int(input())
+# time.sleep(milis / 1000)
+# print('Sample Output:', f'Square root of {number} after {milis} miliseconds is {number ** 0.5}')
+
+# problem 5
+# a = tuple(map(int, input().split()))
+# print(all(a))
---
+++ tsis6/dir-and-files.py
@@ -0,0 +1,61 @@
+import os
+
+# problem 1
+# path = "C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\"
+# files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+# dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+# both = [x for x in os.listdir(path)]
+# print("Files:")
+# print(files)
+# print("Directory:")
+# print(dirs)
+# print("All directories, files:")
+# print(both)
+
+# problem 2
+# print('Exist:', os.access('C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\', os.F_OK))
+# print('Readable:', os.access('C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\', os.R_OK))
+# print('Writable:', os.access('C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\', os.W_OK))
+# print('Executable:', os.access('C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\', os.X_OK))
+
+# problem 3
+# path = "C:\\Users\\Huawei\\OneDrive\\pp2-22B050872\\tsis6\\dir-and-files.py"  # Replace with the actual path you want to test
+# if os.path.exists(path):
+#     print("Path exists.")
+#     filename = os.path.basename(path)
+#     directory = os.path.dirname(path)
+#     print("Filename:", filename)
+#     print("Directory:", directory)
+# else:
+#     print("Path does not exist.")
+
+# problem 4
+# with open("test.txt", "r") as f:
+#     print(sum(1 for _ in f))
+
+# problem 5
+# lista = [123, 23, 53, 23]
+# with open('text.txt', 'w') as f:
+# 	for i in lista:
+# 		f.write('%s\n' % i)
+# content = open('text.txt')
+# print(content.read())
+
+# problem 6
+# list1 = ['A' , 'B' , 'C' , 'D', 'E' , 'F' ,'G' , 'H' ,'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z']
+# for i in list1:
+#     files = f"file_{i}.txt"
+#     with open(files, "w") as file:
+#         file.write('a')
+
+# problem 7 
+# from shutil import copyfile
+# copyfile('test.txt', 'file_Z.txt')
+
+# problem 8 
+# path = 'C:\\Users\\LENOVO\\Desktop\\pp2-22B050871\\'
+# if os.access(path, os.F_OK) and (os.path.isfile('delete.txt')):
+#    os.remove('delete.txt')
+#    print('ok')
+# else:
+#     print(f'{path} does not exist')
